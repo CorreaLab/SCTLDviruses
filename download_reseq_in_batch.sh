9f027aade338cafa3922bab5eb75c24a8c107dbd
aveglia <alex.veglia@upr.edu>
2022-05-09 17:39:07 -0500

    edit

--- download_reseq_in_batch.sh
+++ download_reseq_in_batch.sh
@@ -5,6 +5,8 @@ type="" ##bacter,arch,vert
 amt="" ##number of files
  ##bacteria.3012.1.genomic.fna.gz
 
+amt=$(SAM PUT COOL COMMAND HERE)
+
 for z in $(seq "$amt");
-do  curl -o "$type"."$z".1.genomic.fna.gz https://ftp.ncbi.nlm.nih.gov/refseq/release/bacteria/bacteria."$z".1.genomic.fna.gz    
+do  curl -o "$type"."$z".1.genomic.fna.gz https://ftp.ncbi.nlm.nih.gov/refseq/release/bacteria/bacteria."$z".1.genomic.fna.gz
 done
